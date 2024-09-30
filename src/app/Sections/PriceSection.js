15457e8fd7e1471271c1f1f79d16a202a1894906
miss3persin <ige-bello.oluwasegunfunmi@lmu.edu.ng>
2024-09-30 17:48:32 +0100

    more fixes on the price cards

--- src/app/Sections/PriceSection.js
+++ src/app/Sections/PriceSection.js
@@ -60,9 +60,9 @@ export const PriceSection = () => {
     </div>
 
     {/* MSc */}
-    <div className='bg-[#0A1B33] flex flex-col items-center justify-center text-white py-9 px-6 min-h-[30rem] min-w-[22rem] rounded-lg hover:shadow-[6px_7px_20px_rgba(242,208,202,0.65),-6px_7px_20px_rgba(242,208,202,0.65)] hover:scale-105 transition-all duration-300'>
+    <div className='bg-[#0A1B33] flex flex-col items-center justify-center text-white py-9 px-6 min-h-[30rem] max-w-[22rem] rounded-lg hover:shadow-[6px_7px_20px_rgba(242,208,202,0.65),-6px_7px_20px_rgba(242,208,202,0.65)] hover:scale-105 transition-all duration-300'>
         <p className='font-bold text-xl mb-4'>MSc</p>
-        <p className='font-light text-[0.8rem] md:text-[0.865rem] text-center mb-4 text-[#D0D2D4]'>For typical MSc research projects</p>
+        <p className='font-light text-[0.8rem] md:text-[0.865rem] text-center mb-4 text-[#D0D2D4]'>For typical MSc research projects or projects within that level</p>
         <button className='border-2 cursor-default border-[#FF4B26] w-full font-medium text-[0.865rem] py-3 rounded mb-10 transition duration-200 ease-in-out transform hover:bg-[#FF4B26] hover:scale-105'>Coming Soon</button>
         
         {/* <div className='flex items-center gap-4 mb-3'>
